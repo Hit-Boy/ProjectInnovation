915b5271c5a8b60cbac3926466fb64c142b2f466
ShinigamiAdult <dimitar.bosnew@gmail.com>
2024-02-26 15:35:52 +0100

    Cannon Control script and Cannon prefab added
    Adds 1 Script for controlling the cannon
    by swiping.
    Adds 2 prefabs on for the cannon which is set up with the script and one for a sphere which is used when firing.

---
+++ Assets/Scripts/CannonControl.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CannonControl : MonoBehaviour
+{
+    [SerializeField] private Transform _cannonCart;
+    [SerializeField] private Transform _cannonBarrel;
+    [SerializeField] private Transform _firePoint;
+    [SerializeField] private float _sensitivityX;
+    [SerializeField] private float _sensitivityY;
+    [SerializeField] private float _forceSensitivity;
+    [SerializeField] private GameObject _cannonBall;
+    [SerializeField] private float forceCap;
+    private bool _leftTouch = false;
+    private int leftTouchIndex;
+
+    private bool _rightTouch = false;
+    private int rightTouchIndex;
+
+    private float _fireStrenght;
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(Display.main.systemWidth);
+
+        Touch[] touches = Input.touches;
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            if (touches[i].phase == TouchPhase.Began && !_leftTouch && touches[i].position.x < Display.main.systemWidth / 2)
+            {
+                leftTouchIndex = i;
+                _leftTouch = true;
+                Debug.Log(_leftTouch);
+            }
+
+            if (touches[i].phase == TouchPhase.Began && !_rightTouch && touches[i].position.x > Display.main.systemWidth / 2)
+            {
+                rightTouchIndex = i;
+                _rightTouch = true;
+                Debug.Log(_rightTouch);
+            }
+        }
+
+        //Debug.Log(leftTouch.fingerId);
+        if (Input.touchCount > 0 && touches[leftTouchIndex].phase == TouchPhase.Ended && _leftTouch)
+        {
+            _leftTouch = false;
+            Debug.Log(_leftTouch);
+            //leftTouchIndex = -1;
+        }
+
+        if (Input.touchCount > 0 && touches[rightTouchIndex].phase == TouchPhase.Ended && _rightTouch)
+        {
+            _rightTouch = false;
+            Debug.Log(_rightTouch);
+            FireCannon(_fireStrenght);
+            _fireStrenght = 0;
+            //rightTouchIndex = -1;
+        }
+        if (_leftTouch)
+        {
+            //if (_cannonBarrel.eulerAngles.z <= 0 && _cannonBarrel.eulerAngles.z >= -90)
+            _cannonBarrel.eulerAngles = new Vector3(_cannonBarrel.eulerAngles.x, _cannonBarrel.eulerAngles.y, -touches[leftTouchIndex].deltaPosition.y * _sensitivityY + _cannonBarrel.eulerAngles.z);
+            // else if (_cannonBarrel.eulerAngles.z > 0)
+            //_cannonBarrel.eulerAngles = new Vector3(_cannonBarrel.eulerAngles.x, _cannonBarrel.eulerAngles.y, 0);
+            //else if (_cannonBarrel.eulerAngles.z < -90)
+            //_cannonBarrel.eulerAngles = new Vector3(_cannonBarrel.eulerAngles.x, _cannonBarrel.eulerAngles.y, -90);
+
+            _cannonCart.eulerAngles = new Vector3(_cannonCart.eulerAngles.x, touches[leftTouchIndex].deltaPosition.x * _sensitivityX + _cannonCart.eulerAngles.y, _cannonCart.eulerAngles.z);
+        }
+
+        if (_rightTouch)
+        {
+            _fireStrenght -= touches[rightTouchIndex].deltaPosition.y * _forceSensitivity;
+            if (_fireStrenght < 0)
+                _fireStrenght = 0;
+            else if (_fireStrenght > forceCap)
+                _fireStrenght = forceCap;
+            Debug.Log(_fireStrenght);
+
+        }
+    }
+
+    private void FireCannon(float strength)
+    {
+        GameObject ball = Instantiate(_cannonBall, _firePoint.position, _firePoint.rotation);
+        Rigidbody rb;
+        if (ball.TryGetComponent<Rigidbody>(out rb))
+        {
+            Debug.Log("Rigitbody detected");
+            rb.velocity = _firePoint.right * strength;
+        }
+    }
+}
+
