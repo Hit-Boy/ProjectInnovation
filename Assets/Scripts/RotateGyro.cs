fd723e0b710f8453f65d4653730619d46ba0cccc
ShinigamiAdult <dimitar.bosnew@gmail.com>
2024-03-05 16:12:57 +0100

    Updated changes
    Added gyro cannon, fixed cannon control updated block physics

--- Assets/Scripts/RotateGyro.cs
+++ Assets/Scripts/RotateGyro.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class RotateGyro : MonoBehaviour
 {
+    [SerializeField] private Transform _gyroBase;
+    [SerializeField] private Transform _gyroBarrel;
+    [SerializeField] private Transform _cannonBase;
+    [SerializeField] private Transform _cannonBarrel;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,7 @@ public class RotateGyro : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        _gyroBarrel.rotation = _cannonBarrel.rotation;
+        _gyroBase.rotation = _cannonBase.rotation;
     }
 }
