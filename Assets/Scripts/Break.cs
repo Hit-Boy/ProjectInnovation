3ebd4230f5ecb472e701192aa1c14bb9bd19b565
Theumpp <thomasvegte@hotmail.nl>
2024-03-07 14:41:17 +0100

    Balance Changes

--- Assets/Scripts/Break.cs
+++ Assets/Scripts/Break.cs
@@ -4,6 +4,7 @@ using Unity.VisualScripting;
 using UnityEngine;
 public class Break : MonoBehaviour
 {
+    private float _breakForce = 1.0f;
     private Rigidbody rb;
     private UIManager ui;
     private bool _broken = false;
@@ -17,9 +18,9 @@ public class Break : MonoBehaviour
     }
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.relativeVelocity.magnitude > 1)
+        if(collision.relativeVelocity.magnitude > _breakForce/3)
             rb.isKinematic = false;
-        if(collision.relativeVelocity.magnitude > 3) {
+        if(collision.relativeVelocity.magnitude > _breakForce) {
             if(!_broken) {
                 ui.OnBreak();
                 _broken = true;
