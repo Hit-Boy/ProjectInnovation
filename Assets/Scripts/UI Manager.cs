a9790d4718b774721015130ca91ba82419a08d13
ShinigamiAdult <dimitar.bosnew@gmail.com>
2024-03-08 03:47:39 +0100

    UI Finished

--- Assets/Scripts/UI Manager.cs
+++ Assets/Scripts/UI Manager.cs
@@ -10,18 +10,19 @@ public class UIManager : MonoBehaviour
     [SerializeField] private TMP_Text Score;
     [SerializeField] private TMP_Text Shots;
     [SerializeField] private Slider slider;
+    [SerializeField] private Image image;
 
-    [SerializeField] private ControlCannon controlCannon;
+    [SerializeField] private Sprite[] sprites;
     private int blocksCount;
     private float currentScore;
     private float currentPercentage;
-
     private int shotsFired;
+    private ControlCannon controlCannon;
     void Start() {
+        controlCannon = FindObjectOfType<ControlCannon>();
         slider.minValue = 0;
         slider.maxValue = controlCannon.GetMaxForce();
         blocksCount = FindObjectsOfType<Break>().Length;
-        
         currentScore = 0;
     }
     public void OnBreak() {
@@ -29,10 +30,43 @@ public class UIManager : MonoBehaviour
         currentPercentage = currentScore/blocksCount * 100;
         Score.text = "Destruction: " + Mathf.Ceil(currentPercentage) + "%";
         Debug.Log("CurrentScore: " + currentScore + " BlockCount: " + blocksCount);
+        if(currentPercentage >= 100)
+        {
+            Invoke("EndGame",1.5f);
+            GetComponent<AudioSource>().Play();
+        }
     }
 
     public void OnShoot() {
         shotsFired++;
-         Shots.text = "Times Fired: " + shotsFired;
+         Shots.text = "Fired: " + shotsFired;
+    }
+
+    void EndGame() {
+        controlCannon.enabled = false;
+        Score.gameObject.SetActive(false);
+        image.transform.parent.gameObject.SetActive(true);
+        image.sprite = sprites[0];
+        Invoke("Star1",0.3f);
+    }
+
+    void Star1(){
+        image.sprite = sprites[1];
+        if(shotsFired <= 20)
+            Invoke("Star2",0.3f);
+    }
+
+    void Star2(){
+        image.sprite = sprites[2];
+        if(shotsFired <= 10)
+            Invoke("Star3",0.3f);
+    }
+
+    void Star3(){
+        image.sprite = sprites[3];
+    }
+
+    public void ExitGame() {
+        Application.Quit();
     }
 }
